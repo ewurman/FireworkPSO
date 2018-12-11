32499842fd09707c2877c27e17a0239c26b212e5
ewurman <ewurman@gmail.com>
2018-12-11 14:41:43 -0500

    Adding Rocket.py

--- PythonFirework/Rocket.py
+++ PythonFirework/Rocket.py
@@ -1,9 +1,19 @@
 
 
+
 class Rocket:
 
-    def __init__(self, location, origin, velocity):
+    def __init__(self, location, origin, velocity, evalFunc):
         self.loc = location
         self.origin = origin
         self.velocity = velocity
         self.pbest = list(origin)
+        self.evalFunc = evalFunc
+
+    def launch(self, num_steps):
+        best = self.evaluate(evalFunc)
+
+
+    def evaluate(self, evalFunc):
+        if evalFunc == 
+
